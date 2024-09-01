5ab0a4ee8ebffe695cfccf89ead10c3f4e0d9c1f
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2024-09-01 13:15:30 +0200

    chore: Bump version to 2.4.4
    This commit updates the application version to 2.4.4, reflecting minor changes or bug
     fixes.
    
    The version code is incremented to 34, and the version name is updated in both `build.gradle.kts` and `version.txt`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "cz.jaro.rozvrheditor"
         minSdk = 33
         targetSdk = 34
-        versionCode = 33
-        versionName = "2.4.3"
+        versionCode = 34
+        versionName = "2.4.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
